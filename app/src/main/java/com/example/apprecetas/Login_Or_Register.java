98e5dafee06e4e057f36fc4595f3f72062a3c790
PabloDopico <pdopicob@fpcoruna.afundacion.org>
2023-05-13 23:08:20 +0200

    Añadida pantalla de elección de login y registro, con los botones que te llevan a las actividades (de momento vacías).

--- app/src/main/java/com/example/apprecetas/Login_Or_Register.java
+++ app/src/main/java/com/example/apprecetas/Login_Or_Register.java
@@ -2,7 +2,13 @@ package com.example.apprecetas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.apprecetas.Login.LoginActivity;
+import com.example.apprecetas.Register.RegisterActivity;
 
 public class Login_Or_Register extends AppCompatActivity {
 
@@ -11,6 +17,23 @@ public class Login_Or_Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_or_register_activity);
 
-        
+        Button loginButton = findViewById(R.id.login);
+        Button registerButton = findViewById(R.id.register);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login_Or_Register.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login_Or_Register.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
