8480b3c7de59e8d3dca3f88495fe8cd219f4a6c8
PabloDopicoB <pablodopicoberea@gmail.com>
2023-05-13 21:01:38 +0200

    Creado proyecto definitivo de Android con primera actividad vacía de Login o Registro

---
+++ app/src/main/java/com/example/apprecetas/Login_Or_Register.java
@@ -0,0 +1,14 @@
+package com.example.apprecetas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Or_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_or_register_activity);
+    }
+}
