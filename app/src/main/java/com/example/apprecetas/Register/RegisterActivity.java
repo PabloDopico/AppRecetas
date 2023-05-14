315189dc07677b0491cb3c06dd010137b43cf8ca
PabloDopico <pdopicob@fpcoruna.afundacion.org>
2023-05-14 20:31:53 +0200

    Completado el diseño de la actividad de registro y realizados algunos cambios en los diseños de la app

--- app/src/main/java/com/example/apprecetas/Register/RegisterActivity.java
+++ app/src/main/java/com/example/apprecetas/Register/RegisterActivity.java
@@ -2,8 +2,12 @@ package com.example.apprecetas.Register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.apprecetas.Login.LoginActivity;
 import com.example.apprecetas.R;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -12,5 +16,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView textoIniciarSesion = findViewById(R.id.iniciarSesion);
+
+        textoIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
