ca64457c520a45031b565d3e44fffc89b41df0ff
PabloDopico <pdopicob@fpcoruna.afundacion.org>
2023-05-24 11:18:50 +0200

    añadidas algunas comprobaciones a cerrar sesion y borrar cuenta, y ya funcionan correctamente

--- app/src/main/java/com/example/apprecetas/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/apprecetas/Fragments/SettingsFragment.java
@@ -1,13 +1,16 @@
 package com.example.apprecetas.Fragments;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -15,6 +18,8 @@ import androidx.fragment.app.Fragment;
 import com.example.apprecetas.Login.LoginActivity;
 import com.example.apprecetas.Login_Or_Register;
 import com.example.apprecetas.R;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -58,7 +63,11 @@ public class SettingsFragment extends Fragment {
     }
 
     private void cerrarSesion() {
-        firebaseAuth.signOut();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Cerrar sesión");
+        builder.setMessage("¿Estás segur@ de que deseas cerrar sesión?");
+        builder.setPositiveButton("Sí", (dialog, which) -> {
+            firebaseAuth.signOut();
 
         //guardamos el estado en las sharedpreferences
         SharedPreferences sharedPref = getActivity().getSharedPreferences("preferencias", Context.MODE_PRIVATE);
@@ -69,31 +78,65 @@ public class SettingsFragment extends Fragment {
         Intent intent = new Intent(getActivity(), Login_Or_Register.class);
         startActivity(intent);
         getActivity().finish();
+        });
+        builder.setNegativeButton("No", (dialog, which) -> {
+        });
+        builder.show();
     }
 
     private void borrarCuenta() {
-        FirebaseUser usuario = firebaseAuth.getCurrentUser();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Borrar cuenta");
+        builder.setMessage("¿Estás segur@?");
+        builder.setPositiveButton("Sí", (dialog, which) -> {
+            AlertDialog.Builder contraseñaDialogBuilder = new AlertDialog.Builder(getActivity());
+            contraseñaDialogBuilder.setTitle("Introduce la contraseña");
 
-        if (usuario != null) {
-            usuario.delete().addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(getActivity(), "El usuario se ha borrado correctamente", Toast.LENGTH_SHORT).show();
+            EditText contraseñaEditText = new EditText(getActivity());
+            contraseñaEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            contraseñaDialogBuilder.setView(contraseñaEditText);
 
-                            //guardamos el estado en las sharedpreferences
-                            SharedPreferences sharedPref = getActivity().getSharedPreferences("preferencias", Context.MODE_PRIVATE);
-                            SharedPreferences.Editor editor = sharedPref.edit();
-                            editor.putBoolean("estaLogeado", false);
-                            editor.apply();
+            contraseñaDialogBuilder.setPositiveButton("Eliminar", (dialogContraseña, contraseña1) -> {
+                String contraseña = contraseñaEditText.getText().toString();
 
-                            // al cerrar sesión volvemos a la pantalla de login o registro
-                            Intent intent = new Intent(getActivity(), Login_Or_Register.class);
-                            startActivity(intent);
-                            getActivity().finish();
+                FirebaseUser usuario = firebaseAuth.getCurrentUser();
+                if (usuario != null) {
+                    AuthCredential credential = EmailAuthProvider.getCredential(usuario.getEmail(), contraseña);
+                    usuario.reauthenticate(credential).addOnCompleteListener(
+                            task -> {
+                                if (task.isSuccessful()) {
+                                    usuario.delete().addOnCompleteListener(deleteTask -> {
+                                        if (deleteTask.isSuccessful()) {
+                                            Toast.makeText(getActivity(), "El usuario se ha borrado correctamente", Toast.LENGTH_SHORT).show();
 
-                        } else {
-                            Toast.makeText(getActivity(), "Ha ocurrido un error al borrar la cuenta" + usuario, Toast.LENGTH_SHORT).show();
-                        }
-                    });
-        }
+                                            // Guardamos el estado en las SharedPreferences
+                                            SharedPreferences sharedPref = getActivity().getSharedPreferences("preferencias", Context.MODE_PRIVATE);
+                                            SharedPreferences.Editor editor = sharedPref.edit();
+                                            editor.putBoolean("estaLogeado", false);
+                                            editor.apply();
+
+                                            // Al cerrar sesión volvemos a la pantalla de login o registro
+                                            Intent intent = new Intent(getActivity(), Login_Or_Register.class);
+                                            startActivity(intent);
+                                            getActivity().finish();
+                                        } else {
+                                            Toast.makeText(getActivity(), "Ha ocurrido un error al borrar la cuenta", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                } else {
+                                    Toast.makeText(getActivity(), "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            });
+
+            contraseñaDialogBuilder.setNegativeButton("Cancelar", (dialogContraseña, contraseña1) -> {
+            });
+            contraseñaDialogBuilder.show();
+        });
+        builder.setNegativeButton("No", (dialog, which) -> {
+        });
+        builder.show();
     }
+
 }
