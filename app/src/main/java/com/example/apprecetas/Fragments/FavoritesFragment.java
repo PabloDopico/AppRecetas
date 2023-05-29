01ee6fbbe40bb3ed6858adb1924bfe27329d1e7c
PabloDopico <pdopicob@fpcoruna.afundacion.org>
2023-05-29 12:39:41 +0200

    Añadida opcion para guardar recetas en favoritas, pendiente añadir algunas mejoras

--- app/src/main/java/com/example/apprecetas/Fragments/FavoritesFragment.java
+++ app/src/main/java/com/example/apprecetas/Fragments/FavoritesFragment.java
@@ -1,22 +1,100 @@
 package com.example.apprecetas.Fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.apprecetas.R;
+import com.example.apprecetas.Recipe.Recipe;
+import com.example.apprecetas.Recipe.RecipeActivity;
+import com.example.apprecetas.Recipe.RecipeAdapter;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-public class FavoritesFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class FavoritesFragment extends Fragment implements RecipeAdapter.OnRecipeClickListener {
+
+    private RecyclerView recyclerView;
+    private RecipeAdapter recipeAdapter;
+    private List<Recipe> listaRecetas;
+    private FirebaseFirestore firebaseFirestore;
+    private ProgressBar progressBar;
+    private SharedPreferences sharedPref;
+
     public FavoritesFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_favorites, container, false);
 
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        recyclerView = v.findViewById(R.id.recyclerView);
+        progressBar = v.findViewById(R.id.progressBar);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        listaRecetas = new ArrayList<>();
+        recipeAdapter = new RecipeAdapter(listaRecetas, this);
+        recyclerView.setAdapter(recipeAdapter);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        sharedPref = getActivity().getSharedPreferences("preferencias", Context.MODE_PRIVATE);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        progressBar.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.GONE);
+
+        //obtenemos todas las recetas de Firestore
+        firebaseFirestore.collection("recetas").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // recorremos los documentos y verificamos si cada receta está marcada como favorita
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    String titulo = document.getString("titulo");
+                    String imagen = document.getString("imagen");
+
+                    //comprobamos si la receta esta guardada en las sharedpreferences como favorita
+                    boolean esFavorito = sharedPref.getBoolean(titulo, false);
+
+                    // si esta guardada, la enviamos al constructor
+                    if (esFavorito) {
+                        Recipe receta = new Recipe(titulo, imagen);
+                        listaRecetas.add(receta);
+                    }
+                }
+                recipeAdapter.notifyDataSetChanged();
+            }
+            progressBar.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+        });
+    }
+
+    //metodo para gestionar los clicks en las recetas
+    @Override
+    public void onRecipeClick(int position) {
+        //obtenemos la receta en la posicion clickada
+        Recipe receta = listaRecetas.get(position);
+        String tituloReceta = receta.getTitulo();
+
+        // obtenemos y mandamos el titulo de la receta a la clase RecipeActivity
+        Intent intent = new Intent(getContext(), RecipeActivity.class);
+        intent.putExtra("titulo", tituloReceta);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/apprecetas/Recipe/RecipeActivity.java
+++ app/src/main/java/com/example/apprecetas/Recipe/RecipeActivity.java
@@ -1,150 +1,213 @@
-package com.example.apprecetas.Recipe;
+    package com.example.apprecetas.Recipe;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+    import androidx.annotation.NonNull;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.content.ContextCompat;
 
-import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
+    import android.content.Context;
+    import android.content.SharedPreferences;
+    import android.graphics.drawable.Drawable;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.ImageView;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import com.example.apprecetas.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.squareup.picasso.Picasso;
+    import com.example.apprecetas.R;
+    import com.google.android.gms.tasks.OnCompleteListener;
+    import com.google.android.gms.tasks.Task;
+    import com.google.firebase.firestore.DocumentSnapshot;
+    import com.google.firebase.firestore.FirebaseFirestore;
+    import com.google.firebase.firestore.QueryDocumentSnapshot;
+    import com.google.firebase.firestore.QuerySnapshot;
+    import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-import java.util.Map;
-
-
-public class RecipeActivity extends AppCompatActivity {
-
-    private FirebaseFirestore firestore;
-    private String titulo;
-
-    private ImageView imagenView;
-    private TextView tituloTextView;
-    private TextView descripcionTextView;
-    private TextView ingredientesTextView;
-    private TextView tutorialTextView;
-    private TextView caracteristicasTextView;
-    private TextView valoresNutricionalesTextView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe);
-        firestore = FirebaseFirestore.getInstance();
-
-        //obtenemos el titulo del extra que enviamos desde home fragment
-        titulo = getIntent().getStringExtra("titulo");
-
-        imagenView = findViewById(R.id.imagen);
-        tituloTextView = findViewById(R.id.titulo);
-        descripcionTextView = findViewById(R.id.descripcion);
-        ingredientesTextView = findViewById(R.id.ingredientes);
-        tutorialTextView = findViewById(R.id.tutorial);
-        caracteristicasTextView = findViewById(R.id.caracteristicas);
-        valoresNutricionalesTextView = findViewById(R.id.valoresNutricionales);
-
-        getDatos();
-    }
+    import java.util.ArrayList;
+    import java.util.Map;
 
 
-    private void getDatos() {
+    public class RecipeActivity extends AppCompatActivity {
 
-        //obtenemos de la coleccion recetas el documento que tiene un  titulo que coincide con el que enviamos desde home fragment
-        firestore.collection("recetas").whereEqualTo("titulo", titulo).get().addOnCompleteListener(
-                new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful() && task.getResult() != null && !task.getResult().isEmpty()) {
-                            //mandamos el documento a la clase recipe para crear un objeto
-                            DocumentSnapshot document = task.getResult().getDocuments().get(0);
-                            Recipe receta = document.toObject(Recipe.class);
-                            setDatos(receta);
+        private FirebaseFirestore firestore;
+        private String titulo;
+
+        private ImageView imagenView;
+        private TextView tituloTextView;
+        private TextView descripcionTextView;
+        private TextView ingredientesTextView;
+        private TextView tutorialTextView;
+        private TextView caracteristicasTextView;
+        private TextView valoresNutricionalesTextView;
+        private String recetaNombre;
+        private Button addFavoriteButton;
+        private boolean esFavorito;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_recipe);
+            firestore = FirebaseFirestore.getInstance();
+
+            //obtenemos el titulo del extra que enviamos desde home fragment
+            titulo = getIntent().getStringExtra("titulo");
+
+            imagenView = findViewById(R.id.imagen);
+            tituloTextView = findViewById(R.id.titulo);
+            descripcionTextView = findViewById(R.id.descripcion);
+            ingredientesTextView = findViewById(R.id.ingredientes);
+            tutorialTextView = findViewById(R.id.tutorial);
+            caracteristicasTextView = findViewById(R.id.caracteristicas);
+            valoresNutricionalesTextView = findViewById(R.id.valoresNutricionales);
+
+            addFavoriteButton = findViewById(R.id.botonFavoritos);
+            SharedPreferences sharedPref = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
+            esFavorito = sharedPref.getBoolean(recetaNombre, false);
+            cambiarBotonFavoritos();
+            addFavoriteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    añadirFavoritos();
+                    cambiarBotonFavoritos();
+                }
+            });
+            getDatos();
+        }
+
+
+        private void getDatos() {
+
+            //obtenemos de la coleccion recetas el documento que tiene un  titulo que coincide con el que enviamos desde home fragment
+            firestore.collection("recetas").whereEqualTo("titulo", titulo).get().addOnCompleteListener(
+                    new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful() && task.getResult() != null && !task.getResult().isEmpty()) {
+                                //mandamos el documento a la clase recipe para crear un objeto
+                                DocumentSnapshot document = task.getResult().getDocuments().get(0);
+                                Recipe receta = document.toObject(Recipe.class);
+                                recetaNombre = receta.getTitulo();
+                                setDatos(receta);
+                                cambiarBotonFavoritos();
+                            }
                         }
+                    });
+        }
+
+        private void setDatos(Recipe receta) {
+
+            // con picasso, cargamos la imagen obtenida
+            Picasso.get().load(receta.getImagen()).into(imagenView);
+
+            // establecemos el titulo y descripcion
+            tituloTextView.setText(receta.getTitulo());
+            descripcionTextView.setText(receta.getDescripcion());
+
+            //mediante stringbuilder transformamos la lista a una cadena string normal
+            ArrayList<String> ingredientes = receta.getIngredientes();
+            if (ingredientes != null) {
+                StringBuilder ingredientesText = new StringBuilder();
+
+                for (int i = 0; i < ingredientes.size(); i++) {
+                    String ingrediente = ingredientes.get(i);
+                    //escribimos antes de cada elemento un simbolo
+                    ingredientesText.append("").append(ingrediente);
+                    if (i != ingredientes.size() - 1) {
+                        //damos un salto de linea en cada elemento menos el ultimo
+                        ingredientesText.append("\n");
                     }
-                });
+                }
+                // establecemos el texto al textview de ingredientes
+                ingredientesTextView.setText(ingredientesText.toString());
+            }
+
+            ArrayList<String> tutoriales = receta.getTutorial();
+            if (tutoriales != null) {
+                StringBuilder tutorialesText = new StringBuilder();
+
+                for (int i = 0; i < tutoriales.size(); i++) {
+                    String tutorial = tutoriales.get(i);
+                    tutorialesText.append("⬤  ").append(tutorial);
+
+                    if (i != tutoriales.size() - 1) {
+                        tutorialesText.append("\n\n");
+                    }
+                }
+                tutorialTextView.setText(tutorialesText.toString());
+            }
+
+            ArrayList<String> caracteristicas = receta.getCaracteristicas();
+            if (caracteristicas != null) {
+                StringBuilder caracteristicasText = new StringBuilder();
+                for (int i = 0; i < caracteristicas.size(); i++) {
+                    String caracteristica = caracteristicas.get(i);
+                    caracteristicasText.append("⬤  ").append(caracteristica +"  ⬤");
+                    if (i != caracteristicas.size() - 1) {
+                        caracteristicasText.append("\n");
+                    }
+                }
+                caracteristicasTextView.setText(caracteristicasText.toString());
+            }
+
+            Map<String, String> valoresNutricionales = receta.getValores_nutricionales();
+            if (valoresNutricionales != null && !valoresNutricionales.isEmpty()) {
+                StringBuilder valoresNutricionalesText = new StringBuilder();
+
+                int i = 1;
+                for (Map.Entry<String, String> entry : valoresNutricionales.entrySet()) {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    valoresNutricionalesText.append("").append(key).append(": ").append(value);
+
+                    if (i != valoresNutricionales.size()) {
+                        valoresNutricionalesText.append("\n");
+                    }
+
+                    i++;
+                }
+                valoresNutricionalesTextView.setText(valoresNutricionalesText.toString());
+            } else {
+                valoresNutricionalesTextView.setText("No hay datos disponibles");
+            }
+        }
+
+        private void añadirFavoritos() {
+            esFavorito = !esFavorito;
+            SharedPreferences sharedPref = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putBoolean(recetaNombre, esFavorito);
+            editor.apply();
+            cambiarBotonFavoritos();
+
+            String mensaje;
+            if (esFavorito) {
+                mensaje = "Agregada a favoritos";
+            } else {
+                mensaje = "Eliminada de favoritos";
+            }
+
+            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+        }
+
+        private void cambiarBotonFavoritos() {
+            SharedPreferences sharedPref = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
+            esFavorito = sharedPref.getBoolean(recetaNombre, false);
+
+            Drawable drawable;
+            if (esFavorito) {
+                drawable = ContextCompat.getDrawable(this, R.drawable.corazon);
+
+            } else {
+                drawable = ContextCompat.getDrawable(this, R.drawable.corazon_roto2);
+            }
+
+            if (drawable != null) {
+                drawable.setTint(ContextCompat.getColor(this, R.color.white));
+                addFavoriteButton.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+            }
+        }
+        @Override
+        protected void onResume() {
+            super.onResume();
+            cambiarBotonFavoritos();
+        }
     }
-
-    private void setDatos(Recipe receta) {
-
-        // con picasso, cargamos la imagen obtenida
-        Picasso.get().load(receta.getImagen()).into(imagenView);
-
-        // establecemos el titulo y descripcion
-        tituloTextView.setText(receta.getTitulo());
-        descripcionTextView.setText(receta.getDescripcion());
-
-        //mediante stringbuilder transformamos la lista a una cadena string normal
-        ArrayList<String> ingredientes = receta.getIngredientes();
-        if (ingredientes != null) {
-            StringBuilder ingredientesText = new StringBuilder();
-
-            for (int i = 0; i < ingredientes.size(); i++) {
-                String ingrediente = ingredientes.get(i);
-                //escribimos antes de cada elemento un simbolo
-                ingredientesText.append("").append(ingrediente);
-                if (i != ingredientes.size() - 1) {
-                    //damos un salto de linea en cada elemento menos el ultimo
-                    ingredientesText.append("\n");
-                }
-            }
-            // establecemos el texto al textview de ingredientes
-            ingredientesTextView.setText(ingredientesText.toString());
-        }
-
-        ArrayList<String> tutoriales = receta.getTutorial();
-        if (tutoriales != null) {
-            StringBuilder tutorialesText = new StringBuilder();
-
-            for (int i = 0; i < tutoriales.size(); i++) {
-                String tutorial = tutoriales.get(i);
-                tutorialesText.append("⬤  ").append(tutorial);
-
-                if (i != tutoriales.size() - 1) {
-                    tutorialesText.append("\n\n");
-                }
-            }
-            tutorialTextView.setText(tutorialesText.toString());
-        }
-
-        ArrayList<String> caracteristicas = receta.getCaracteristicas();
-        if (caracteristicas != null) {
-            StringBuilder caracteristicasText = new StringBuilder();
-            for (int i = 0; i < caracteristicas.size(); i++) {
-                String caracteristica = caracteristicas.get(i);
-                caracteristicasText.append("⬤  ").append(caracteristica +"  ⬤");
-                if (i != caracteristicas.size() - 1) {
-                    caracteristicasText.append("\n");
-                }
-            }
-            caracteristicasTextView.setText(caracteristicasText.toString());
-        }
-
-        Map<String, String> valoresNutricionales = receta.getValores_nutricionales();
-        if (valoresNutricionales != null && !valoresNutricionales.isEmpty()) {
-            StringBuilder valoresNutricionalesText = new StringBuilder();
-
-            int i = 1;
-            for (Map.Entry<String, String> entry : valoresNutricionales.entrySet()) {
-                String key = entry.getKey();
-                String value = entry.getValue();
-                valoresNutricionalesText.append("").append(key).append(": ").append(value);
-
-                if (i != valoresNutricionales.size()) {
-                    valoresNutricionalesText.append("\n");
-                }
-
-                i++;
-            }
-            valoresNutricionalesTextView.setText(valoresNutricionalesText.toString());
-        } else {
-            valoresNutricionalesTextView.setText("No hay datos disponibles");
-        }
-    }
-}
